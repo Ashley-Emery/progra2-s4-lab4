f0542724d50ccb7879bf0b817b060c3acd7fa194
Ashley Fajardo <a.e.fajardo06@gmail.com>
2025-11-07 11:17:16 -0600

    refactor de clase AdminPalabrasSecretas

--- src/progra2/s4/lab4/AdminPalabrasSecretas.java
+++ src/progra2/s4/lab4/AdminPalabrasSecretas.java
@@ -3,32 +3,70 @@ package progra2.s4.lab4;
 
 import java.util.ArrayList;
 import java.util.Random;
+import java.util.Collections;
 
 /**
  *
  * @author axelr
  */
 public class AdminPalabrasSecretas {
-    ArrayList<String> listaPalabras= new ArrayList<>();
     
-    AdminPalabrasSecretas(ArrayList<String> listaPalabras){
+    public ArrayList<String> listaPalabras= new ArrayList<>();
+    public Random rnd;
+    
+    public AdminPalabrasSecretas(){
         this.listaPalabras= listaPalabras;
+        this.rnd = new Random();
+        
+        Collections.addAll(this.listaPalabras, "JAVA", "PROGRAMACION", "AHORCADO", "NETBEANS", "UNIVERSIDAD", "ASHLEY", "AXEL", "CARLOS", "GUILLERMO");
     }
     
-    public void agregarPalabras(String nuevaPalabra){
-        for( String palabra : listaPalabras){
-            if(nuevaPalabra.equals(palabra)==false)
-                listaPalabras.add(nuevaPalabra);
+        public boolean agregarPalabra(String palabra) {
+            
+        if (palabra == null)
+            return false;
+        
+        String p = palabra.trim();
+        
+        if (p.isEmpty())
+            return false;
+        
+        for (String existente : listaPalabras) {
+            if (existente.equalsIgnoreCase(p)) {
+                return false;
+            }
         }
+        
+        listaPalabras.add(p.toUpperCase());
+        return true;
     }
     
-    public String obtenerPalabraFija(int indice){
-        return listaPalabras.get(indice-1);
+    public String obtenerPalabraAlAzar() {
+        
+        if (listaPalabras.isEmpty())
+            return null;
+        return listaPalabras.get(rnd.nextInt(listaPalabras.size()));
     }
     
-    public String obtenerPalabraAzar(){
-        Random indice= new Random();
-        return listaPalabras.get(indice.nextInt(0, listaPalabras.size()));
+    public ArrayList<String> listarPalabras() {
+        
+        return new ArrayList<>(listaPalabras);
+    }
+
+    public boolean removerPalabra(String palabra) {
+        
+        if (palabra == null)
+            return false;
+        
+        for (int i = 0; i < listaPalabras.size(); i++) {
+            
+            if (listaPalabras.get(i).equalsIgnoreCase(palabra.trim())) {
+                
+                listaPalabras.remove(i);
+                return true;
+            }
+        }
+        return false;
     }
     
 }
