3a64230a1e994ccca758491ea3bf05e86098acdb
Ashley Fajardo <a.e.fajardo06@gmail.com>
2025-11-07 08:50:04 -0600

    crear metodo crearFiguraBase

--- src/progra2/s4/lab4/JuegoAhorcadoBase.java
+++ src/progra2/s4/lab4/JuegoAhorcadoBase.java
@@ -10,6 +10,7 @@ package progra2.s4.lab4;
  */
 
 import java.util.ArrayList;
+import java.util.Arrays;
 
 public abstract class JuegoAhorcadoBase {
     
@@ -25,6 +26,26 @@ public abstract class JuegoAhorcadoBase {
         this.letrasUsadas = new ArrayList<>();
         this.figuraAhorcado = crearFiguraBase();
         this.intentos = this.limiteIntentos;
+    }
+    
+    public JuegoAhorcadoBase(int limiteIntentos){
+        this();
+        if( limiteIntentos>0 ){
+            this.limiteIntentos = limiteIntentos;
+            this.intentos = limiteIntentos;
+        }
+    }
+    
+    public ArrayList<String> crearFiguraBase(){
+        return new ArrayList<>(Arrays.asList(
+                "",
+                "  O  ",
+                "  O  \\n  |  ",
+                "  O  \\n /|  ",
+                "  O  \\n /|\\\\ ",
+                "  O  \\n /|\\\\ \\n /   ",
+                "  O  \\n /|\\\\ \\n / \\\\ "
+        ));
     }
     
     
