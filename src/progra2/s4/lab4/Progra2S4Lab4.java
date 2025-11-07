b15acd063c41d8c56dbeaf4336697a889528c6bd
Ashley Fajardo <a.e.fajardo06@gmail.com>
2025-11-07 08:07:20 -0600

    initial commit

---
+++ src/progra2/s4/lab4/Progra2S4Lab4.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package progra2.s4.lab4;
+
+/**
+ *
+ * @author ashley
+ */
+public class Progra2S4Lab4 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
