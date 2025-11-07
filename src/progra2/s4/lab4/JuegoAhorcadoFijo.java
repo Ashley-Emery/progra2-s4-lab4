d40faad1a156ca4d277a744eed3be427cd01176e
carlos_guardado@unitec.edu <carlos_guardado@unitec.edu>
2025-11-07 10:59:15 -0600

    últimos cambios en la clase JuegoAHorcadoFijo para adptarlo al codigo implementado en la ClaseBase Abstracta

--- src/progra2/s4/lab4/JuegoAhorcadoFijo.java
+++ src/progra2/s4/lab4/JuegoAhorcadoFijo.java
@@ -4,25 +4,37 @@ package progra2.s4.lab4;
 public class JuegoAhorcadoFijo extends JuegoAhorcadoBase{
     
     public JuegoAhorcadoFijo(String palabra){
-        super (palabra);
+        super (6);
+        inicializarPalabraSecreta(palabra);
     }
 
-    protected boolean verificarLetra(){
-        return palabraSecreta.contains(String.valueOf(letter));
+    @Override
+    public boolean verificarLetra(char letra){
+        return palabraSecreta.contains(String.valueOf(letra));
     }
     
-    protected void actualizarPalabraActual(char letra){
+    @Override
+    public void actualizarPalabraActual(char letra){
+        
+        StringBuilder sb = new StringBuilder(palabraActual);
+                
         for (int i = 0 ; i < palabraSecreta.length(); i ++){
             if (palabraSecreta.charAt(i) == letra){
                 
-                palabraActual.setCharAt(i, letra); 
+                sb.setCharAt(i, letra); 
             }
         }
+        palabraActual = sb.toString();
     }
     
+    @Override
     public boolean hasGanado(){
-        return !palabraActual.toString().contains("   ");
+        return !palabraActual.contains("");
     }
     
+    @Override
+    public void jugar(){
+        
+    }
     
 }
