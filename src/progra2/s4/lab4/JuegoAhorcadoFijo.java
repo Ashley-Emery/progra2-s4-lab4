3f1f4c7e8d14d8652ad6680b4c18db22cfb8e7fb
Ashley Fajardo <a.e.fajardo06@gmail.com>
2025-11-07 11:22:56 -0600

    refactor de clase JuegoAhorcadoFijo

--- src/progra2/s4/lab4/JuegoAhorcadoFijo.java
+++ src/progra2/s4/lab4/JuegoAhorcadoFijo.java
@@ -4,25 +4,53 @@ package progra2.s4.lab4;
 public class JuegoAhorcadoFijo extends JuegoAhorcadoBase{
     
     public JuegoAhorcadoFijo(String palabra){
-        super (palabra);
+        super();
+        inicializarPalabraSecreta(palabra);
+        
+        if (figuraAhorcado == null || figuraAhorcado.isEmpty()) {
+            figuraAhorcado = crearFiguraBase();
+        }
+        
     }
 
-    protected boolean verificarLetra(){
-        return palabraSecreta.contains(String.valueOf(letter));
-    }
-    
-    protected void actualizarPalabraActual(char letra){
-        for (int i = 0 ; i < palabraSecreta.length(); i ++){
-            if (palabraSecreta.charAt(i) == letra){
-                
-                palabraActual.setCharAt(i, letra); 
+    public boolean verificarLetra(char letra) throws AhorcadoExceptions.EntradaInvalidaException{
+        if (!Character.isLetter(letra)) {
+            throw new AhorcadoExceptions.EntradaInvalidaException("Debe ingresar una letra.");
+        }
+        letra = Character.toUpperCase(letra);
+        boolean contiene = false;
+        for (int i = 0; i < palabraSecreta.length(); i++) {
+            if (palabraSecreta.charAt(i) == letra) {
+                contiene = true;
             }
         }
+        if (contiene) {
+            actualizarPalabraActual(letra);
+        }
+        return contiene;
     }
     
-    public boolean hasGanado(){
-        return !palabraActual.toString().contains("   ");
+    public void actualizarPalabraActual(char letra){
+        
+        StringBuilder sb = new StringBuilder(palabraActual);
+        
+        for (int i = 0; i < palabraSecreta.length(); i++) {
+            
+            if (palabraSecreta.charAt(i) == letra) {
+                sb.setCharAt(i, letra);
+            }
+        }
+        palabraActual = sb.toString();
     }
     
+    public boolean hasGanado() {
+        return palabraActual != null && palabraActual.equals(palabraSecreta);
+    }
+    
+    public void jugar() {
+        throw new UnsupportedOperationException("Use la GUI para jugar (VentanaAhorcado).");
+    }
+    
+    public void actualizarFigura(){}
     
 }
