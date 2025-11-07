281aa943ddad32480e57675e5a3987ee0f9cd2fd
Ashley Fajardo <a.e.fajardo06@gmail.com>
2025-11-07 08:37:36 -0600

    crear atributos

---
+++ src/progra2/s4/lab4/JuegoAhorcadoBase.java
@@ -0,0 +1,28 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package progra2.s4.lab4;
+
+/**
+ *
+ * @author ashley
+ */
+
+import java.util.ArrayList;
+
+public abstract class JuegoAhorcadoBase {
+    
+    protected String palabraSecreta;
+    protected String palabraActual;
+    protected int intentos;
+    protected int limiteIntentos;
+    protected ArrayList<Character> letrasUsadas;
+    protected ArrayList<String> figuraAhorcado;
+    
+    public JuegoAhorcadoBase(){
+        
+        
+        
+    }
+}
